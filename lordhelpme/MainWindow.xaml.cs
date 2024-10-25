17dfcf44139d7fe48a487ba144b6737c391c2be3
janslan <janko.slanina@hotmail.com>
2024-10-25 17:31:45 +0100

    Fire progress with UI and scripting

--- lordhelpme/MainWindow.xaml.cs
+++ lordhelpme/MainWindow.xaml.cs
@@ -7,10 +7,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -23,6 +27,12 @@ namespace lordhelpme
     {
         private DispatcherTimer _timer;
         public const string bottomBarAboutMe = "This application has been developed by lunarprogramas - V.0.1b";
+        public const Boolean signedIn = false;
+        public const Boolean forceTestError = false;
+
+        DataTable users = new DataTable();
+       
+
         public MainWindow()
         {
             this.InitializeComponent();
@@ -30,6 +40,29 @@ namespace lordhelpme
             this.BottomBar.Text = bottomBarAboutMe;
             Debug.Write("Started interface module!");
             this.StartClock();
+
+            if (forceTestError)
+            {
+                this.errorPage.Visibility = Visibility.Visible;
+                this.signedOutMenu.Visibility = Visibility.Collapsed;
+            } else
+            {
+                this.signedOutMenu.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void initiateUsers()
+        {
+            users.Clear();
+            users.Columns.Add("Id", typeof(String));
+            users.Columns.Add("Password", typeof(String)); // sorry for no encryption i cba
+
+            DataRow row = users.NewRow();
+            row["Id"] = "admin";
+            row["Password"] = "admin";
+            users.Rows.Add(row);
+
+            Debug.WriteLine("Successfully loaded UserAccountService!");
         }
 
         private void StartClock()
@@ -44,7 +77,16 @@ namespace lordhelpme
 
         private void Timer_Tick(object sender, object e)
         {
-            this.Clock.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            
+            if (this.signedOutMenu.Visibility == Visibility.Visible)
+            {
+                this.ClockSignedOutMenu.Text = DateTime.Now.ToString("hh:mm tt");
+                this.Clock.Visibility = Visibility.Collapsed;
+            } else
+            {
+                this.Clock.Visibility = Visibility.Visible;
+                this.Clock.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            }
         }
 
         private void ViewLoginPage(object sender, RoutedEventArgs e)
@@ -55,8 +97,26 @@ namespace lordhelpme
 
         private void ReturnToMenu(object sender, RoutedEventArgs e)
         {
-            this.signedOutMenu.Visibility = Visibility.Visible;
-            this.loginPage.Visibility= Visibility.Collapsed;
+            if (this.errorPage.Visibility == Visibility.Visible)
+            {
+                this.signedOutMenu.Visibility = Visibility.Visible;
+                this.errorPage.Visibility = Visibility.Collapsed;
+            } else
+            {
+                this.signedOutMenu.Visibility = Visibility.Visible;
+                this.loginPage.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void LogIn(object sender, RoutedEventArgs e)
+        {
+                if (this.username.Text.Length == 0 || this.password.Password.Length == 0)
+                {
+                    Debug.WriteLine("No logged user!");
+                    this.incorrectCredentialsNotice.Visibility = Visibility.Visible;
+                    await Task.Delay(10000);
+                    this.incorrectCredentialsNotice.Visibility = Visibility.Collapsed;
+                }
         }
     }
 
